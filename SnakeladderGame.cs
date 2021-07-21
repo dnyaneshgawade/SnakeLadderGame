37ad511f73b135a44ce41cd3a34f6285041e8e35
dnyana <dnyaneshgawade903@gmail.com>
2021-07-21 12:38:30 +0530

    [Dnyanesh] add: repeat till winning

--- SnakeladderGame.cs
+++ SnakeladderGame.cs
@@ -6,34 +6,38 @@ namespace SnakeLadder
 {
     public class SnakeladderGame
     {
-        int pos = 0;
+        public int pos = 0;
         public void game()
         {
-            Console.WriteLine("current position =" + pos);
-            Random ran = new Random();
-            int dice = ran.Next(1, 7);
-            Console.WriteLine("player got : "+dice);
-            int choice = ran.Next(0, 3);
-            switch (choice)
+            while(pos<100)
             {
-                case 0:
-                    Console.WriteLine("you got ladder");
-                    pos = pos + dice;
-                    Console.WriteLine("position after dice =" + pos);
-                    break;
-                case 1:
-                    Console.WriteLine("you got snake");
-                    pos = pos-dice;
-                    if (pos < 0)
-                        pos = 0;
-                    Console.WriteLine("position after dice =" + pos);
-                    break;
-                default:
-                    Console.WriteLine("you got nothing ");
-                    Console.WriteLine("position after dice =" + pos);
-                    break;
-            }
+                Console.WriteLine("current position =" + pos);
+                Random ran = new Random();
+                int dice = ran.Next(1, 7);
+                Console.WriteLine("player got : " + dice);
+                int choice = ran.Next(0, 3);
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("you got ladder");
+                        pos = pos + dice;
+                        Console.WriteLine("position after dice =" + pos);
+                        break;
+                    case 1:
+                        Console.WriteLine("you got snake");
+                        pos = pos - dice;
+                        if (pos < 0)
+                            pos = 0;
+                        Console.WriteLine("position after dice =" + pos);
+                        break;
+                    default:
+                        Console.WriteLine("you got nothing ");
+                        Console.WriteLine("position after dice =" + pos);
+                        break;
+                }
+            } 
         }
+        
         
     }
 }
