50263d18e9212263fe197339961cf5d84ef9042c
dnyana <dnyaneshgawade903@gmail.com>
2021-07-21 12:13:32 +0530

    [Dnyanesh] add: check for options

--- SnakeladderGame.cs
+++ SnakeladderGame.cs
@@ -13,6 +13,26 @@ namespace SnakeLadder
             Random ran = new Random();
             int dice = ran.Next(1, 7);
             Console.WriteLine("player got : "+dice);
+            int choice = ran.Next(0, 3);
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("you got ladder");
+                    pos = pos + dice;
+                    Console.WriteLine("position after dice =" + pos);
+                    break;
+                case 1:
+                    Console.WriteLine("you got snake");
+                    pos = pos-dice;
+                    if (pos < 0)
+                        pos = 0;
+                    Console.WriteLine("position after dice =" + pos);
+                    break;
+                default:
+                    Console.WriteLine("you got nothing ");
+                    Console.WriteLine("position after dice =" + pos);
+                    break;
+            }
         }
         
     }
